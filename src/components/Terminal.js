ba10f4c98d8e4437c0fbf8b42dc11775b09eb127
Faraz Shaikh <farazzshaikh@gmail.com>
2021-08-20 17:13:12 +0400

    Conneted backend to frontend

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -38,12 +38,31 @@ export function Terminal() {
     setInput(e.target.value);
   }
 
-  function onKeyDown(e) {
+  async function onKeyDown(e) {
     if (e.key === "Enter") {
-      const output = `Command not found: ${input}`;
+      if (input === "clear") {
+        setLog([]);
+      } else {
+        try {
+          const response = await fetch("http://localhost:8000/", {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+              CMD: input,
+            }),
+          });
 
-      const _log = [{ output, input }, ...log];
-      setLog(_log);
+          const { Stdout, Stderr } = await response.json();
+
+          const _log = [{ output: Stdout || Stderr, input }, ...log];
+          setLog(_log);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
       e.target.value = "";
       setInput("");
     }
@@ -52,8 +71,14 @@ export function Terminal() {
   useEffect(() => {
     if (inputRef.current) {
       inputRef.current.focus();
+      //   inputRef.current.scrollIntoView({ behavior: "smooth" });
+      var page = document.querySelector("body");
+      var scrollable = document.querySelector("main.terminal ul");
+      var scrolled = document.querySelector(".cursor input");
+      page.scrollTop = scrollable.offsetTop - page.offsetTop;
+      scrollable.scrollTop = scrolled.offsetTop - scrollable.offsetTop;
     }
-  }, [inputRef]);
+  }, [inputRef, log]);
 
   return (
     <div className="terminal-container">
@@ -62,6 +87,8 @@ export function Terminal() {
 
         <div className="titlebar">p2prc</div>
         <ul>
+          <li className="seperator"></li>
+          <li className="seperator"></li>
           <li className="cursor">
             <span className="home-indicator">{"➜"}</span>
             <input
@@ -75,8 +102,6 @@ export function Terminal() {
           <Runtime />
 
           {generateLog(log)}
-          <li className="seperator"></li>
-          <li className="seperator"></li>
         </ul>
       </main>
     </div>
